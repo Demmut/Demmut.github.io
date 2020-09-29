26474d55036d5661e19242577ac98938f513415c
Demmut <uddarendeli@gmail.com>
2020-09-29 17:35:10 +0200

    Updates

---
+++ precache-manifest.352e6512ca8ef9d880ea51a9b0bae429.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1dbf578498ccb67e4760acb17cf393",
+    "url": "/Demmut.github.io/index.html"
+  },
+  {
+    "revision": "0eeac9665698b8a3456f",
+    "url": "/Demmut.github.io/static/css/main.2fbb319a.chunk.css"
+  },
+  {
+    "revision": "afd0e4ce4b53ba552f76",
+    "url": "/Demmut.github.io/static/js/2.3e5ab2b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Demmut.github.io/static/js/2.3e5ab2b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eeac9665698b8a3456f",
+    "url": "/Demmut.github.io/static/js/main.d5f208b9.chunk.js"
+  },
+  {
+    "revision": "e479e4dd59726c26308e",
+    "url": "/Demmut.github.io/static/js/runtime-main.f2adbd8f.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Demmut.github.io/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "cecfd51c29c277f36a9caf7769a79b9b",
+    "url": "/Demmut.github.io/static/media/Peddana-Regular.cecfd51c.ttf"
+  },
+  {
+    "revision": "2d3925bf49cac7ba5fd457f6842d2373",
+    "url": "/Demmut.github.io/static/media/jap_waves.2d3925bf.png"
+  }
+]);
